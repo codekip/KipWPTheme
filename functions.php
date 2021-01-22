2e962eab59627799e9a1ce045c6fad8fe505d108
Nick Sawe <codekip@outlook.com>
2021-01-22 23:03:22 +0000

    Made stylesheet version dynamic

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function kiptheme_register_styles(){
-    wp_enqueue_style('kiptheme-style', get_template_directory_uri() . "/style.css",array('kiptheme-bootstrap'),'1.0','all');
+    $version = wp_get_theme()->get( 'Version' );
+    wp_enqueue_style('kiptheme-style', get_template_directory_uri() . "/style.css",array('kiptheme-bootstrap'),$version,'all');
     wp_enqueue_style('kiptheme-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",array(),'1.0','all');
     wp_enqueue_style('kiptheme-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css",array(),'1.0','all');
 }
