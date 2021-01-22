9e2089fed567184ccb32d75bba66a21df2abb2a9
Nick Sawe <codekip@outlook.com>
2021-01-22 22:55:05 +0000

    Got styles from github to match tutorial https://github.com/wilsmex/blog-site-template

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function kiptheme_register_styles(){
+    wp_enqueue_style('kiptheme-style', get_template_directory_uri() . "/style.css",array('kiptheme-bootstrap'),'1.0','all');
+    wp_enqueue_style('kiptheme-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",array(),'1.0','all');
+    wp_enqueue_style('kiptheme-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css",array(),'1.0','all');
+}
+
+add_action( 'wp_enqueue_scripts', 'kiptheme_register_styles');
+
+?>
