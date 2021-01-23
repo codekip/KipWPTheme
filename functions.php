217dc2d2e32d145c98f9cb9c6973e09c3795c99b
CodeKip <codekip@outlook.com>
2021-01-23 17:01:29 +0000

    Revert "Small edit to functions.php"

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 
-function kiptheme_register_styles()
-{
-    $version = wp_get_theme()->get('Version');
-    wp_enqueue_style('kiptheme-style', get_template_directory_uri() . "/style.css", array('kiptheme-bootstrap'), $version, 'all');
-    wp_enqueue_style('kiptheme-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '1.0', 'all');
-    wp_enqueue_style('kiptheme-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '1.0', 'all');
+function kiptheme_register_styles(){
+    $version = wp_get_theme()->get( 'Version' );
+    wp_enqueue_style('kiptheme-style', get_template_directory_uri() . "/style.css",array('kiptheme-bootstrap'),$version,'all');
+    wp_enqueue_style('kiptheme-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",array(),'1.0','all');
+    wp_enqueue_style('kiptheme-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css",array(),'1.0','all');
 }
 
+add_action( 'wp_enqueue_scripts', 'kiptheme_register_styles');
 
-add_action('wp_enqueue_scripts', 'kiptheme_register_styles');
+?>
